b712e4e86b96f0365c6f1fc708918e0d1b714c19
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2024-12-27 15:28:53 +0300

    fix

---
+++ pages/test.vue
@@ -0,0 +1,47 @@
+<template>
+    <main class="main">
+          <!-- Здесь может быть ваш основной контент страницы -->
+        <p>Подключение к Яндексу...</p>
+    </main>
+</template>
+
+<!-- @/assets/images/logo.png -->
+<script>
+
+
+
+
+export default {
+  data(){
+    return{
+
+
+    }
+  },
+  components: {
+
+  },
+  methods: {
+
+  },
+
+  mounted() {
+    // Скрипт Яндекса подключается, когда компонент загружается
+    const script = document.createElement("script");
+    script.src = "https://yastatic.net/s3/passport-sdk/autofill/v1/sdk-suggest-token-with-polyfills-latest.js";
+    script.async = true;
+    script.onload = () => {
+      // Скрипт загружен, теперь можно вызвать функцию
+      if (window.YaSendSuggestToken) {
+        window.YaSendSuggestToken("https://natalnaya-karta-online.ru", {
+          "kek": true
+        });
+      } else {
+        console.error("YaSendSuggestToken не определена");
+      }
+    };
+    document.head.appendChild(script);
+
+  },
+};
+</script>

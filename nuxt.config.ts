483222336a941cd6ff197eb2f79683819ddddbcf
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-01-16 18:32:30 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' }
+      ]
+    },
+  },
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],

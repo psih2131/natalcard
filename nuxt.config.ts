4363a2eabeccf7c8b0e321d222c44ad839722e52
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-01-20 19:58:57 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
-        { name: 'Content-Security-Policy', content: `default-src https://google.com https://www.google.com https://pay.yandex.ru https://api-statist.dev-tcsgroup.io https://api-statist.tinkoff.ru https://forma.tinkoff.ru; connect-src https://google.com https://www.google.com https://pay.yandex.ru https://api-statist.dev-tcsgroup.io https://api-statist.tinkoff.ru https://forma.tinkoff.ru https://widget.cloudpayments.ru; frame-ancestors 'self' https://widget.cloudpayments.ru;` },
+
       ],
 
     },

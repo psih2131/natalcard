831335a491a1b9e4d491c9b68ac97ce31ba1752e
Roman Kuzmich <87143625+psih2131@users.noreply.github.com>
2025-01-21 20:21:00 +0300

    fix new

--- middleware/security.ts
+++ middleware/security.ts
@@ -1,10 +1,10 @@
 export default defineEventHandler((event) => {
     appendHeader(event, 'Content-Security-Policy', [
         "default-src 'self';",
-        "script-src 'self' https://yandex.ru https://*.yandex.ru;",
+        "script-src 'self' https://yandex.ru https://*.yandex.ru https://widget.cloudpayments.ru;",
         "frame-ancestors 'self' https://yandex.ru https://*.yandex.ru;",
-        "connect-src 'self' https://yandex.ru https://*.yandex.ru;",
+        "connect-src 'self' https://yandex.ru https://*.yandex.ru https://widget.cloudpayments.ru;",
         "style-src 'self' 'unsafe-inline';",
-        "img-src 'self' data: https://yandex.ru https://*.yandex.ru;",
+        "img-src 'self' data: https://yandex.ru https://*.yandex.ru https://natalbase.ru/;",
     ].join(' '));
 });
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,64 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' },
-        { name: 'Content-Security-Policy', content: `default-src https://google.com https://www.google.com https://pay.yandex.ru https://api-statist.dev-tcsgroup.io https://api-statist.tinkoff.ru https://forma.tinkoff.ru; connect-src https://google.com https://www.google.com https://pay.yandex.ru https://api-statist.dev-tcsgroup.io https://api-statist.tinkoff.ru https://forma.tinkoff.ru https://widget.cloudpayments.ru; frame-ancestors 'self' https://widget.cloudpayments.ru;` },
+
       ],
 
     },
   },
 
+  security: {
+    headers: {
+      contentSecurityPolicy: {
+        'img-src': ["*"],
+
+        'default-src': [
+          "https://google.com",
+          "https://www.google.com",
+          "https://pay.yandex.ru",
+          "https://api-statist.dev-tcsgroup.io",
+          "https://api-statist.tinkoff.ru",
+          "https://forma.tinkoff.ru",
+        ],
+        'connect-src': [
+          "'self'", // Разрешает текущий домен
+          "https://api.cloudpayments.ru", // Добавляем домен CloudPayments
+          'https://widget.cloudpayments.ru/feature-api/config',  // Правильный путь к API
+          'https://widget.cloudpayments.ru',
+          "https://natalbase.ru",
+          "https://autofill.yandex.ru",
+          "https://google.com",
+          "https://www.google.com",
+          "https://pay.yandex.ru",
+          "https://api-statist.dev-tcsgroup.io",
+          "https://api-statist.tinkoff.ru",
+          "https://forma.tinkoff.ru",
+        ],
+
+        'frame-src': [
+          "'self'",
+          'https://widget.cloudpayments.ru', // Разрешаем загрузку фреймов с этого домена
+        ],
+
+
+      },
+
+    },
+
+
+
+  },
+
+
+
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
-  modules: ['@pinia/nuxt'],
-  serverHandlers: [
-    { route: '/api', handler: '~/middleware/security.ts' },
-  ],
+  modules: ['@pinia/nuxt', 'nuxt-security'],
+  // serverHandlers: [
+  //   { route: '/api', handler: '~/middleware/security.ts' },
+  // ],
+
+
 });
